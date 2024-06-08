91d6c5d598c8914764d10b978e9ec1289b793c3d
LUVIKXDDDDDDD <106072726+gametaherrtv12333@users.noreply.github.com>
2024-06-08 18:19:38 +0400

    Add files via upload

---
+++ Ru_ru_clicker.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.Scanner;
+
+public class Ru_ru_clicker {
+    public static void main(String[] args)throws AWTException {
+        Robot bot = new Robot();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Здравствуйте! Введите задержку между кликами");
+        int Delay = scan.nextInt();
+        System.out.println("Выберите количество сделаных кликов Бесконечность Если 0");
+        int Clicks = scan.nextInt();
+        int i = 0;
+        bot.delay(2000);
+        if (Clicks == 0){
+            bot.mousePress(InputEvent.BUTTON1_MASK);
+            bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        }
+        while (i < Clicks){
+            bot.delay(Delay);
+            bot.mousePress(InputEvent.BUTTON1_MASK);
+            bot.mouseRelease(InputEvent.BUTTON1_MASK);
+            i += 1;
+        }
+
+    }
+}
