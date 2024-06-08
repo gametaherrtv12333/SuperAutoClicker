bc3b8c13f7ab9a2a30981a35f280e0b12e9f213b
LUVIKXDDDDDDD <106072726+gametaherrtv12333@users.noreply.github.com>
2024-06-08 18:18:16 +0400

    Add files via upload

---
+++ En_US_clicker.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+import java.util.Scanner;
+
+public class En_US_clicker {
+    public static void main(String[] args)throws AWTException {
+        Robot bot = new Robot();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello! Enter delay between clicks(ms)");
+        int Delay = scan.nextInt();
+        System.out.println("Choose how many clicks will be made endlessly if 0");
+        int Clicks = scan.nextInt();
+        int i = 0;
+        bot.delay(2000);
+        if (Clicks == 0){
+            bot.mousePress(InputEvent.BUTTON1_MASK);
+            bot.mouseRelease(InputEvent.BUTTON1_MASK);
+        }
+        while (i < Clicks){
+            bot.delay(Delay);
+            bot.mousePress(InputEvent.BUTTON1_MASK);
+            bot.mouseRelease(InputEvent.BUTTON1_MASK);
+            i += 1;
+        }
+
+    }
+}
